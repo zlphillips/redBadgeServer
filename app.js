419193b945b63243d9220905c29618dcc8258ddd
zlphillips <60951969+zlphillips@users.noreply.github.com>
2020-07-13 19:22:31 -0400

    zach server update

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var sequelize = require('./db');
 
 sequelize.sync();
 app.use(express.json());
+app.use(adminBro.options.rootPath, router)
+app.listen(8080, () => console.log('AdminBro is under localhost:8080/admin'))
 app.use(require('./middleware/headers'));
 app.use('/redBadge/user', user);
 app.use('/redBadge/post', post)
